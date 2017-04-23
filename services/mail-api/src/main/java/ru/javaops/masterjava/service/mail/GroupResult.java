c6123459c10dc8459530b1a3a1def075469e80c0
Mikhail Zherdev <michaelzherdev@gmail.com>
2017-04-23 11:33:33 +0300

    lesson 08

--- services/mail-api/src/main/java/ru/javaops/masterjava/service/mail/GroupResult.java
+++ services/mail-api/src/main/java/ru/javaops/masterjava/service/mail/GroupResult.java
@@ -1,10 +1,13 @@
 package ru.javaops.masterjava.service.mail;
 
+import com.google.common.base.Throwables;
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.util.List;
 
+@Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class GroupResult {
@@ -12,10 +15,14 @@ public class GroupResult {
     private List<MailResult> failed; // failed emails with causes
     private String failedCause;  // global fail cause
 
+    public GroupResult(Exception e) {
+        this(-1, null, Throwables.getRootCause(e).toString());
+    }
+
     @Override
     public String toString() {
         return "Success: " + success + '\n' +
-                "Failed: " + failed.toString() + '\n' +
-                (failedCause == null ? "" : "Failed cause" + failedCause);
+                (failed == null ? "" : "Failed: " + failed.toString() + '\n') +
+                (failedCause == null ? "" : "Failed cause: " + failedCause);
     }
 }
--- services/mail-service/src/test/java/ru/javaops/masterjava/service/mail/MailServiceClient.java
+++ services/mail-service/src/test/java/ru/javaops/masterjava/service/mail/MailServiceClient.java
@@ -1,6 +1,7 @@
 package ru.javaops.masterjava.service.mail;
 
 import com.google.common.collect.ImmutableSet;
+import ru.javaops.web.WebStateException;
 
 import javax.xml.namespace.QName;
 import javax.xml.ws.Service;
@@ -16,12 +17,19 @@ public class MailServiceClient {
 
         MailService mailService = service.getPort(MailService.class);
 
-        mailService.sendToGroup(ImmutableSet.of(
+        ImmutableSet<Addressee> addressees = ImmutableSet.of(
                 new Addressee("gkislin@javaops.ru"),
-                new Addressee("Bad Email <bad_email.ru>")), ImmutableSet.of(), "Subject", "Body");
+                new Addressee("Мастер Java <masterjava@javaops.ru>"),
+                new Addressee("Bad Email <bad_email.ru>"));
 
-        mailService.sendToGroup(
-                ImmutableSet.of(new Addressee("Григорий Кислин <gkislin@javaops.ru>")),
-                ImmutableSet.of(new Addressee("Мастер Java <masterjava@javaops.ru>")), "Subject", "Body");
+        try {
+            String status = mailService.sendToGroup(addressees, ImmutableSet.of(), "Bulk email subject", "Bulk email body");
+            System.out.println(status);
+
+            GroupResult groupResult = mailService.sendBulk(addressees, "Individual mail subject", "Individual mail body");
+            System.out.println(groupResult);
+        } catch (WebStateException e) {
+            System.out.println(e);
+        }
     }
 }
