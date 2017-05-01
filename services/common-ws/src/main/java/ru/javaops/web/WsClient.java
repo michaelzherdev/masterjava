2122b18ffcc0bd943338edf95bb54e06552e5ecf
Mikhail Zherdev <michaelzherdev@gmail.com>
2017-05-01 20:25:11 +0300

    lesson09

--- services/common-ws/src/main/java/ru/javaops/web/WsClient.java
+++ services/common-ws/src/main/java/ru/javaops/web/WsClient.java
@@ -5,9 +5,13 @@ import ru.javaops.masterjava.ExceptionType;
 import ru.javaops.masterjava.config.Configs;
 
 import javax.xml.namespace.QName;
+import javax.xml.ws.Binding;
 import javax.xml.ws.BindingProvider;
 import javax.xml.ws.Service;
+import javax.xml.ws.WebServiceFeature;
+import javax.xml.ws.handler.Handler;
 import java.net.URL;
+import java.util.List;
 import java.util.Map;
 
 public class WsClient<T> {
@@ -31,12 +35,25 @@ public class WsClient<T> {
     }
 
     //  Post is not thread-safe (http://stackoverflow.com/a/10601916/548473)
-    public T getPort() {
-        T port = service.getPort(serviceClass);
+    public T getPort(WebServiceFeature... features) {
+        T port = service.getPort(serviceClass, features);
         BindingProvider bp = (BindingProvider) port;
         Map<String, Object> requestContext = bp.getRequestContext();
         requestContext.put(BindingProvider.ENDPOINT_ADDRESS_PROPERTY, endpointAddress);
         return port;
+    }
+
+    public static <T> void setAuth(T port, String user, String password) {
+        Map<String, Object> requestContext = ((BindingProvider) port).getRequestContext();
+        requestContext.put(BindingProvider.USERNAME_PROPERTY, user);
+        requestContext.put(BindingProvider.PASSWORD_PROPERTY, password);
+    }
+
+    public static <T> void setHandler(T port, Handler handler) {
+        Binding binding = ((BindingProvider) port).getBinding();
+        List<Handler> handlerList = binding.getHandlerChain();
+        handlerList.add(handler);
+        binding.setHandlerChain(handlerList);
     }
 
     public static WebStateException getWebStateException(Exception e) {
--- services/mail-api/src/main/java/ru/javaops/masterjava/service/mail/MailWSClient.java
+++ services/mail-api/src/main/java/ru/javaops/masterjava/service/mail/MailWSClient.java
@@ -5,15 +5,25 @@ import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Iterables;
 import com.google.common.io.Resources;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.event.Level;
+import ru.javaops.web.AuthUtil;
 import ru.javaops.web.WebStateException;
 import ru.javaops.web.WsClient;
+import ru.javaops.web.handler.SoapClientLoggingHandler;
 
 import javax.xml.namespace.QName;
+import javax.xml.ws.soap.MTOMFeature;
+import java.util.List;
 import java.util.Set;
 
 @Slf4j
 public class MailWSClient {
     private static final WsClient<MailService> WS_CLIENT;
+    public static final String USER = "user";
+    public static final String PASSWORD = "password";
+    private static final SoapClientLoggingHandler LOGGING_HANDLER = new SoapClientLoggingHandler(Level.DEBUG);
+
+    public static String AUTH_HEADER = AuthUtil.encodeBasicAuthHeader(USER, PASSWORD);
 
     static {
         WS_CLIENT = new WsClient<MailService>(Resources.getResource("wsdl/mailService.wsdl"),
@@ -24,11 +34,11 @@ public class MailWSClient {
     }
 
 
-    public static String sendToGroup(final Set<Addressee> to, final Set<Addressee> cc, final String subject, final String body) throws WebStateException {
+    public static String sendToGroup(final Set<Addressee> to, final Set<Addressee> cc, final String subject, final String body, List<Attach> attaches) throws WebStateException {
         log.info("Send mail to '" + to + "' cc '" + cc + "' subject '" + subject + (log.isDebugEnabled() ? "\nbody=" + body : ""));
         String status;
         try {
-            status = WS_CLIENT.getPort().sendToGroup(to, cc, subject, body);
+            status = getPort().sendToGroup(to, cc, subject, body, attaches);
             log.info("Sent with status: " + status);
         } catch (Exception e) {
             log.error("sendToGroup failed", e);
@@ -37,17 +47,24 @@ public class MailWSClient {
         return status;
     }
 
-    public static GroupResult sendBulk(final Set<Addressee> to, final String subject, final String body) throws WebStateException {
+    public static GroupResult sendBulk(final Set<Addressee> to, final String subject, final String body, List<Attach> attaches) throws WebStateException {
         log.info("Send mail to '" + to + "' subject '" + subject + (log.isDebugEnabled() ? "\nbody=" + body : ""));
         GroupResult result;
         try {
-            result = WS_CLIENT.getPort().sendBulk(to, subject, body);
+            result = getPort().sendBulk(to, subject, body, attaches);
         } catch (WebStateException e) {
             log.error("sendBulk failed", e);
             throw WsClient.getWebStateException(e);
         }
         log.info("Sent with result: " + result);
         return result;
+    }
+
+    private static MailService getPort() {
+        MailService port = WS_CLIENT.getPort(new MTOMFeature(1024));
+        WsClient.setAuth(port, USER, PASSWORD);
+        WsClient.setHandler(port, LOGGING_HANDLER);
+        return port;
     }
 
     public static Set<Addressee> split(String addressees) {
